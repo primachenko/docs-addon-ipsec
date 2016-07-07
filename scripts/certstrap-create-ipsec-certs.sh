6f922003858c6f94b8be8a2f271e4cd1b6894354
Ben Tarnoff <btarnoff@pivotal.io>
2016-07-07 15:00:00 -0700

    update ipsec installation topic with info on how to generate self-signed certs[#121518393]

---
+++ scripts/certstrap-create-ipsec-certs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# A script to create IPsec certificates using certstrap tool.
+# see:  https://github.com/square/certstrap/blob/master/README.md
+#
+# certstrap utility will prompt for passwords to protect the keys.  Enter whatever you want.
+#
+set -o errexit
+
+if [ -e certs ]; then
+    rm -rf certs
+fi
+
+mkdir certs
+cd certs
+
+git clone https://github.com/square/certstrap
+cd certstrap/
+./build
+
+./bin/certstrap init --common-name PCF\ IPsec\ AddOn\ CA
+
+./bin/certstrap request-cert --common-name PCF\ IPsec\ peer
+
+./bin/certstrap sign PCF\ IPsec\ peer --CA PCF\ IPsec\ AddOn\ CA
+
+openssl x509 -in out/PCF_IPsec_AddOn_CA.crt -inform pem -text
+openssl x509 -in out/PCF_IPsec_peer.crt -inform pem -text
+
+rm -rf ./out/PCF_IPsec_peer.csr
+
+echo " "
+echo "New IPsec certificates created in ./certs/certstrap/out subdirectory:"
+echo " "
+
+ls -la out
+
+cd ..
